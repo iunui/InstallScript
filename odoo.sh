35be3434ad4f1bb4a05e5353b038f0c14520e4b9
iunui <30671755+iunui@users.noreply.github.com>
2018-01-10 02:08:51 +0700

    Update odoo.sh

--- odoo.sh
+++ odoo.sh
@@ -28,7 +28,7 @@ OE_PORT="8069"
 #IMPORTANT! This script contains extra libraries that are specifically needed for Odoo 11.0
 OE_VERSION="11.0"
 # Set this to True/False if you want to install Odoo 11 Enterprise!
-IS_ENTERPRISE="True"
+IS_ENTERPRISE="False"
 #set the superadmin password
 OE_SUPERADMIN="0908510260"
 OE_CONFIG="${OE_USER}-server"
@@ -37,16 +37,16 @@ OE_CONFIG="${OE_USER}-server"
 #--------------------------------------------------
 echo -e "\n---- Update Server ----"
 sudo apt-get update
-sudo apt-get upgrade -y
+sudo apt-get -y upgrade
 
 echo -e "\n---- Install WKHTMLTOPDF ----"
-sudo apt-get install wkhtmltopdf
+sudo apt-get install -y wkhtmltopdf
 
 #--------------------------------------------------
 # Install PostgreSQL Server
 #--------------------------------------------------
 echo -e "\n---- Install PostgreSQL Server ----"
-sudo apt-get install postgresql -y
+sudo apt-get install -y postgresql
 
 echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
 sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
@@ -54,24 +54,24 @@ sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 #--------------------------------------------------
 # Install Dependencies
 #--------------------------------------------------
-echo -e "\n--- Installing Python 3 + pip3 --"
-sudo apt-get install python3 python3-pip
-
 echo -e "\n---- Install tool packages ----"
-sudo apt-get install wget git bzr python-pip gdebi-core -y
+sudo apt-get install -y wget git bzr python-pip gdebi-core
+
+echo -e "\n--- Installing Python 3 + pip3 --"
+sudo apt-get install -y python3 python3-pip
 
 echo -e "\n---- Install python packages ----"
-sudo apt-get install python-pypdf2 python-dateutil python-feedparser python-ldap python-libxslt1 python-lxml python-mako python-openid python-psycopg2 python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi python-docutils python-psutil python-mock python-unittest2 python-jinja2 python-pypdf python-decorator python-requests python-passlib python-pil -y
-sudo pip3 install pypdf2 Babel passlib Werkzeug decorator python-dateutil pyyaml psycopg2 psutil html2text docutils lxml pillow reportlab ninja2 requests gdata XlsxWriter vobject python-openid pyparsing pydot mock mako Jinja2 ebaysdk feedparser xlwt psycogreen suds-jurko pytz pyusb greenlet xlrd 
+sudo apt-get install -y python-pypdf2 python-dateutil python-feedparser python-ldap python-libxslt1 python-lxml python-mako python-openid python-psycopg2 python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi python-docutils python-psutil python-mock python-unittest2 python-jinja2 python-pypdf python-decorator python-requests python-passlib python-pil
+sudo pip3 install -y pypdf2 Babel passlib Werkzeug decorator python-dateutil pyyaml psycopg2 psutil html2text docutils lxml pillow reportlab ninja2 requests gdata XlsxWriter vobject python-openid pyparsing pydot mock mako Jinja2 ebaysdk feedparser xlwt psycogreen suds-jurko pytz pyusb greenlet xlrd 
 
 echo -e "\n---- Install python libraries ----"
 # This is for compatibility with Ubuntu 16.04. Will work on 14.04, 15.04 and 16.04
-sudo apt-get install python3-suds
+sudo apt-get install -y python3-suds
 
 echo -e "\n--- Install other required packages"
-sudo apt-get install node-clean-css -y
-sudo apt-get install node-less -y
-sudo apt-get install python-gevent -y
+sudo apt-get install -y node-clean-css
+sudo apt-get install -y node-less
+sudo apt-get install -y python-gevent
 
 echo -e "\n---- Create ODOO system user ----"
 sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'ODOO' --group $OE_USER
@@ -109,7 +109,7 @@ if [ $IS_ENTERPRISE = "True" ]; then
     echo -e "\n---- Added Enterprise code under $OE_HOME/enterprise/addons ----"
     echo -e "\n---- Installing Enterprise specific libraries ----"
     sudo pip3 install num2words ofxparse
-    sudo apt-get install nodejs npm
+    sudo apt-get install -y nodejs npm
     sudo npm install -g less
     sudo npm install -g less-plugin-clean-css
 fi
